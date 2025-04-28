c69e9d24839b0d53e222e2600f0f066af5a5ff6f
hao.chen8 <hc222ig@student.lnu.se>
2025-04-28 16:46:09 +0200

    Update API base URL to use environment variable

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://sp500-charts.onrender.com/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 export default api;
